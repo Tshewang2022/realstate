10e0fdd3495d8297bf2c4c7bbd32f4b52646b7c1
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-07-21 17:49:30 +0600

    it is the part of the fun project

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,22 @@
+<script setup>
+import {ref} from 'vue';
+import {useRoute} from 'vue-router';
+import { Icon } from '@iconify/vue';
+import { Disclosure, DisclosureButton, } from '@headlessui/vue'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
+
+const showProfile = ref(false);
+const route = useRoute();
+
+
+</script>
 <template>
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -22,7 +41,7 @@
             </div>
           </div>
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <button @click="showProfile=true" type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
               <span class="absolute -inset-1.5" />
               <span class="sr-only">View notifications</span>
               <BellIcon class="h-6 w-6" aria-hidden="true" />
@@ -38,7 +57,10 @@
               </div>
               <div enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                 <!-- needs to render it conditionally -->
-                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" id="profile">
+                <div v-if="showProfile" class="absolute right-0 z-20 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-index" >
+                  <div @click="showProfile=false" class='flex items-end justify-end p-2'>
+                   <Icon icon="mdi:close"/>
+                  </div>
                   <div>
                     <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
                   </div>
@@ -48,6 +70,7 @@
                   <div >
                     <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
                   </div>
+                 
                 </div>
             </div>
         </div>
@@ -63,22 +86,4 @@
     </Disclosure>
   </template>
   
-  <script setup>
-  import {ref} from 'vue';
-  import {useRoute} from 'vue';
-  import { Disclosure, DisclosureButton, } from '@headlessui/vue'
-  import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-  
-  const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-  ]
-
-  const profile = ref(false);
-  const showProfile = ()=>{
-
-  }
-
-  </script>
+ 
